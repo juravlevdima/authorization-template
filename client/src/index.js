bf643a8976a0361706052d7a259e026a75d742ea
juravlevdima <juravlevdima96@gmail.com>
2022-08-25 16:51:21 +0600

    prettier sort plugin

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,13 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import { BrowserRouter } from "react-router-dom"
 import { Provider } from "react-redux"
+import { BrowserRouter } from "react-router-dom"
 
+import App from "./App.js"
 import store from "./redux/index.js"
 
 import "./css/tailwind.css" // tailwind
 import "./css/style.scss" // custom styles
-
-import App from "./App.js"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
